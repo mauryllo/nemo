18926cb8434f04bb2294774e638ef057f436825b
mauryllo <mauryllo@gmail.com>
2018-11-12 16:20:44 +0100

    12/11/2018 16:17

--- nemo.py
+++ nemo.py
@@ -15,44 +15,60 @@ import time
 from classes import Host, PollProfile
 from pyng import ping
 
+maxthreads=2
+semaphore_poll_icmp=threading.Semaphore(value=maxthreads)
+
 def poll_icmp(host, poll_profile):
-	if host.dns:
-		ip=host.hostname
-	else:
-		ip=host.ip
-		
 	try:
-		start=datetime.datetime.now()
+		#wait till the semaphore goes green
+		semaphore_poll_icmp.acquire()
+		
+		if host.dns:
+			ip=host.hostname
+		else:
+			ip=host.ip
+
 		payload_size = 100
 		result=ping(ip, poll_profile.timeout, payload_size)
-		print(host.ip,result)
-		#wait before next ping
-		stop=datetime.datetime.now()-start
-		waitfor="{:.2f}".format(poll_profile.interval-((stop.seconds*1000000 + stop.microseconds)/1000000))
-		time.sleep(float(waitfor))
+		semaphore_poll_icmp.release()
 	except Exception as e:
 		raise e
 	
 	return result
-
+	
 def poller(hosts, poll_profiles):
 	#initialize result
 	result=dict()
 	for host in hosts:
 		result[host]=[]
 	
-	threads=[]
-	
-	#create threads
-	for i in range(3):
+	#poll interval in seconds
+	poll_interval = 5
+
+	while True:
+		start=datetime.datetime.now()
+		#set up threads
+		threads=[]
 		for host in hosts:
 			try:
 				poll_profile_id=hosts[host].poll_profile_id
-				t = threading.Thread(target=poll_icmp, args=(hosts[host],poll_profiles[poll_profile_id]))
-				threads.append(t)
-				t.start()
+				threads.append(threading.Thread(target=poll_icmp, args=(hosts[host],poll_profiles[poll_profile_id])))
 			except Exception as e:
 				logging.error("Host \"" + hosts[host].hostname + "\": " + str(e))
+				
+		#start all threads
+		for t in threads:
+			t.start()
+		
+		#wait for all threads to finish
+		for t in threads:
+			t.join()
+			print(result)
+			
+		#wait before next pool cycle
+		stop=datetime.datetime.now()-start
+		waitfor="{:.2f}".format(poll_interval-((stop.seconds*1000000 + stop.microseconds)/1000000))
+		time.sleep(float(waitfor))
 	
 #	for i in range(3):
 #		for host in hosts:
@@ -81,6 +97,7 @@ def main(argv=None):
 		hosts[1]=Host(hostname, dns=False, ip=ip, poll_profile_id=1)
 		hosts[2]=Host("www.google.com", dns=True, poll_profile_id=1)
 		hosts[3]=Host("www.nonfunge.com", dns=True, poll_profile_id=1)
+		hosts[4]=Host("c7609.ca-02", dns=False, ip="213.205.0.15", poll_profile_id=1)
 	except Exception as e:
 		logging.warning("Host \"" + hostname + "\": " + str(e))
 	
